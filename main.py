891f5d7dcd27af7be4e3e5536c0dbc98fb02d31c
JHOANN ENIRQUE ACOSTA AMARANTO <jeacosta37@gmail.com>
2025-03-20 20:24:01 -0500

    update conection def users to main
    se realizo conexion de funciones de usuarios con el archivo main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 ## aqui va todo el llamado a cada una de la funciones de los codigos
+
+
+from flask import Flask,jsonify
+import usuarios,ingresos,dashboard,gastos
+
+app = Flask(__name__)
+
+@app.route('/api/users', methods=['GET'])
+def get_all_users():
+     return usuarios.get_all_users()
+
+@app.route('/api/user/<int:Cedula>', methods=['GET'])
+def get_one_user(Cedula):
+     return usuarios.get_one_user(Cedula)
+
+@app.route('/api/usuarios', methods=['POST'])
+def create_user():
+     return usuarios.create_user()
+
+
+@app.route('/api/users/<int:Cedula>', methods=['PUT'])
+def update_user(Cedula):
+     return usuarios.update_user()
+
+@app.route('/api/users/<int:Cedula>', methods=['DELETE'])
+def delete_user(Cedula):
+     return usuarios.delete_user
+
+if __name__ == '__main__':
+    app.run(debug=True)
